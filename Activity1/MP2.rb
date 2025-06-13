4aae9b51950e04e74809aff7775204674c5135fe
samchvs <chavessamantha019@gmail.com>
2025-06-13 11:57:03 +0800

    concatenation

--- Activity1/MP2.rb
+++ Activity1/MP2.rb
@@ -1,1 +1,6 @@
-puts "Hello world"
+#Create two variables and concatenate like your first name and a last name.
+
+firstname = "Sam"
+lastname = "Chaves"
+
+puts "My name is #{firstname} #{lastname}"

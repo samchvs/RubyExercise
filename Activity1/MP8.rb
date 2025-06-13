882a4d28515d2289b100b16107806d8772f14f82
samchvs <chavessamantha019@gmail.com>
2025-06-13 12:39:43 +0800

    gets.to_i usage

---
+++ Activity1/MP8.rb
@@ -0,0 +1,13 @@
+#Create two variables: one for your birth year and one for the current year. Calculate and print your age.
+
+print "Enter your birth year: "
+birth_year = gets.to_i
+
+print "Enter the current year: "
+current_year = gets.to_i
+
+age = current_year-birth_year
+
+puts "Your age is #{age}"
+
+

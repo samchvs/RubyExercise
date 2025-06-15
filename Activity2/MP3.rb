e8eda372774f8d41a5be6ba12eff267f53e701b9
samchvs <chavessamantha019@gmail.com>
2025-06-15 18:34:50 +0800

    printing a specific index

---
+++ Activity2/MP3.rb
@@ -0,0 +1,8 @@
+#Create an array that contains the days of the week, from Monday to Sunday. Print the third day in the array.
+
+def printWeek
+    array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    puts array[2]
+end
+
+printWeek

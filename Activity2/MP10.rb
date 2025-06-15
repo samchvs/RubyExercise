c84c370e9169ee9c3f61e08b896fd821e3ccff9c
samchvs <chavessamantha019@gmail.com>
2025-06-15 19:17:59 +0800

    using array.index() to find the index of "Mexico"

---
+++ Activity2/MP10.rb
@@ -0,0 +1,10 @@
+#You have an array of countries, Find and print the index of "Mexico".
+#["USA", "Canada", "Mexico", "Germany", "France"]
+
+def findIndex
+    array = ["USA", "Canada", "Mexico", "Germany", "France"]
+    arrayIndex = array.index("Mexico")
+
+    puts "#{arrayIndex}"
+end
+findIndex

8999f953364a28ecf51f334611fa338fe262f163
samchvs <chavessamantha019@gmail.com>
2025-06-15 18:50:30 +0800

    sorting array ascendingly using .sort method

---
+++ Activity2/MP5.rb
@@ -0,0 +1,11 @@
+#Given the following array, sort the array in ascending order and print the result.
+#[2, 5, 3, 8, 1]
+
+def sortArray
+    array = [2, 5, 3, 8, 1]
+    sortedArray = array.sort
+
+    puts sortedArray.inspect
+end
+
+sortArray

66228fa2460af893e1bfd82ecc14e183f42c0338
samchvs <chavessamantha019@gmail.com>
2025-06-15 19:03:48 +0800

    .length method

---
+++ Activity2/MP7.rb
@@ -0,0 +1,9 @@
+#Create an array of your 5 favorite movies. Print the length of the array.
+
+def faveMovies
+    array = ["Interstellar", "Shawshank Redemption", "The Green Mile", "Shutter Island", "Me Before You"]
+    result = array.length
+
+    puts "#{result}"
+end
+faveMovies

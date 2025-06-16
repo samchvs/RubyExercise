0cea2a76370a7b451c054e259e265b3ab41cf907
samchvs <chavessamantha019@gmail.com>
2025-06-16 18:07:46 +0800

    combining two arrays into one

---
+++ Activity2/MP14.rb
@@ -0,0 +1,10 @@
+#You have two arrays: ["red", "green", "blue"] and ["yellow", "purple", "pink"] . Combine them into one array and print the result.
+
+def combineArray
+    array1 = ["red", "green", "blue"] 
+    array2 = ["yellow", "purple", "pink"]
+    combinedArray = array1 + array2
+
+    puts "#{combinedArray}"
+end
+combineArray

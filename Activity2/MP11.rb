9aaf20c989bbf27621718e0d50f040d100d8d7bf
samchvs <chavessamantha019@gmail.com>
2025-06-16 13:07:13 +0800

    print the last name in the array

---
+++ Activity2/MP11.rb
@@ -0,0 +1,7 @@
+#Create an array with the names of 4 family members. Print the last name in the array.
+
+def names
+    array = ["Shirley", "Joey", "Diego", "Samantha", "Sophia", "Sasha"]
+    puts array[5]
+end
+names

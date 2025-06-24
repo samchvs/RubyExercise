a08986f5f3efd9b1ce7a210516bafdb13b906543
samchvs <chavessamantha019@gmail.com>
2025-06-24 23:20:49 +0800

    Write a program that asks for a person's age and checks if they are a teenager (between 13 and 19). If they are, print You are a teenager.

--- Activity3/MP10.rb
+++ Activity3/MP10.rb
@@ -1,4 +1,4 @@
-# Write a program that asks for a person's age and checks if they are a teenager (between 13 and 19). If they are, print You are a teenager.
+#Write a program that asks for a person's age and checks if they are a teenager (between 13 and 19). If they are, print You are a teenager.
 
 def checkAge
     puts "Enter age: "
